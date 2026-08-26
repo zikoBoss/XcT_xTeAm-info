d02fc9263a5886eb8076da8747459dcf8b4707ea
ZikoB0SS <faromtb2006@gmail.com>
2026-08-26 21:32:21 +0100

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,1 +1,8 @@
+import os
+import sys
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+from xAyOuB_app import xAyOuB_app as app
